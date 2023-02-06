4d1ae166aa9b46a85cfd55d51020c8d345988a1d
srujankumarpinamwar49 <86636377+srujankumarpinamwar49@users.noreply.github.com>
2023-02-06 00:37:02 -0700

    Update PasswordGeneratorSequence.py
    Generating password both in sequence and random order

--- section5/PasswordGeneratorSequence.py
+++ section5/PasswordGeneratorSequence.py
@@ -1,4 +1,3 @@
-# Go to: https://replit.com/@appbrewery/password-generator-start?v=1
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -9,16 +8,65 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# password = ""
+
+# number_of_letters = 0
+# number_of_symbols = 0
+# number_of_numbers = 0
+
+
+
+# for n in range(1, nr_letters+1):
+#     if nr_letters and number_of_letters == nr_letters:
+#         password = password
+#     else:
+#         password = password + random.choice(letters)
+#         number_of_letters + 1
+
+# for n in range(1, nr_symbols+1):
+#     if nr_symbols and number_of_symbols == nr_symbols:
+#         password = password
+#     else:
+#         password = password + str(random.choice(symbols))
+#         number_of_symbols + 1
+
+# for n in range(1, nr_numbers+1):
+#     if nr_numbers and number_of_numbers == nr_numbers:
+#         password = password
+#     else:
+#         password = password + str(random.choice(numbers))
+#         number_of_numbers + 1
+
+
+# print(password)
+# random.shuffle(password_list)
+# print(password_list)
+
+# password = ""
+# for char in password_list:
+#   password += char
+
+# print(f"Your password is: {password}")
+
+
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
 password = ""
+for char in password_list:
+  password += char
 
-for n in range(1, nr_letters+1):
-    password = password + random.choice(letters)
-
-for n in range(1, nr_symbols+1):
-    password = password + str(random.choice(symbols))
-
-for n in range(1, nr_numbers+1):
-    password = password + str(random.choice(numbers))
-
-print(password)
-
+print(f"Your password is: {password}")
